d98e49dd81a84845e88eca205428bc357744452a
Vachok <143500@gmail.com>
2018-08-11 13:35:11 +0300

    feat(/docs mapping):

--- src/main/java/ru/vachok/networker/IndexController.java
+++ src/main/java/ru/vachok/networker/IndexController.java
@@ -1,9 +1,16 @@
 package ru.vachok.networker;
 
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
 
 
 @Controller
@@ -14,5 +21,14 @@ public class IndexController {
       ModelAndView modelAndView = new ModelAndView();
       modelAndView.addObject("Name", "Vachok");
       return modelAndView;
+   }
+
+
+   @RequestMapping("/docs")
+   @ResponseBody
+   public String addrInLocale( HttpServletRequest httpServletRequest ) throws IOException {
+
+
+      return "redirect:https://vachok.testquality.com/project/3260/plan/6672/test/86686" + "\n" + Arrays.toString(new UnknownError().getStackTrace()).replaceAll(", " , "\n");
    }
 }
